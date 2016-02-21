124e069c018ac351314220cdc0eb3284ffdee339
Ryan Warner <warner.ryan@gmail.com>
2016-02-21 12:30:27 -0800

    add eyes and mouth

--- Happiness/Happiness/FaceView.swift
+++ Happiness/Happiness/FaceView.swift
@@ -60,7 +60,52 @@ class FaceView: UIView
 		static let FaceRadiusToMouthOffsetRatio: CGFloat = 3
 	}
 	
+	private enum Eye { case Left, Right }
 	
+	private func bezierPathForEye( whichEye: Eye) -> UIBezierPath
+	{
+		let eyeRadius = faceRadius / Scaling.FaceRadiusToEyeRadiusRatio
+		let eyeVerticalOffset = faceRadius / Scaling.FaceRadiusToEyeOffsetRatio
+		let eyeHorizontalSeparation = faceRadius / Scaling.FaceRadiusToEyeSeparationRatio
+		
+		var eyeCenter = faceCenter
+		eyeCenter.y -= eyeVerticalOffset
+		
+		switch whichEye
+		{
+			case .Left: eyeCenter.x -= eyeHorizontalSeparation / 2
+			case .Right: eyeCenter.x += eyeHorizontalSeparation / 2
+		}
+		
+		let path = UIBezierPath( arcCenter: eyeCenter,
+								    radius: eyeRadius,
+								startAngle: 0,
+								  endAngle: CGFloat( 2 * M_PI ),
+								clockwise: true )
+
+		path.lineWidth = lineWidth
+		return path
+	}
+	
+	private func bezierPathForSmile( fractionOfMaxSmile: Double) -> UIBezierPath
+	{
+		let mouthWidth = faceRadius / Scaling.FaceRadiusToMouthWidthRatio
+		let mouthHeight = faceRadius / Scaling.FaceRadiusToMouthHeightRatio
+		let mouthVerticalOffset = faceRadius / Scaling.FaceRadiusToMouthOffsetRatio
+		
+		let smileHeight = CGFloat(max(min(fractionOfMaxSmile, 1), -1)) * mouthHeight
+		
+		let start = CGPoint( x: faceCenter.x - mouthWidth / 2, y: faceCenter.y + mouthVerticalOffset )
+		let end = CGPoint( x: start.x + mouthWidth, y: start.y )
+		let cp1 = CGPoint( x: start.x + mouthWidth / 3, y: start.y + smileHeight )
+		let cp2 = CGPoint( x: end.x - mouthWidth / 3, y: cp1.y )
+		
+		let path = UIBezierPath()
+		path.moveToPoint(start)
+		path.addCurveToPoint(end, controlPoint1: cp1, controlPoint2: cp2)
+		path.lineWidth = lineWidth
+		return path
+	}
 
     override func drawRect(rect: CGRect)
 	{
@@ -72,6 +117,14 @@ class FaceView: UIView
 		facePath.lineWidth = lineWidth
 		color.set()
 		facePath.stroke()
+		
+		bezierPathForEye(.Left).stroke()
+		bezierPathForEye(.Right).stroke()
+		
+		let smiliness = 0.33
+		let smilePath = bezierPathForSmile(smiliness)
+		
+		smilePath.stroke()
     }
 
 }
